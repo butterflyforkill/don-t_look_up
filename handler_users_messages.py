5f3463cbdb8b0d78e60051c903ccf597b0f9c848
butterflyforkill <vasse.paul@gmail.com>
2024-07-22 17:23:00 +0200

    Add fucntionality for sending the messages to the user

--- handler_users_messages.py
+++ handler_users_messages.py
@@ -4,33 +4,47 @@
 # sending the SMS to the user
 
 import requests
+from datetime import date
+from dateutil import parser
+
+
+APP_NAME = "DONT_LOOK_UP"
 
 
 def response_handler(response):
-    pass
+    if response == 200:
+        print("message has been sent")
+    else:
+        print("message hasn't been sent")
+    
 
 
 def get_messages():
     response = requests.get("http://hackathons.masterschool.com:3030/team/getMessages/DONT_LOOK_UP")
-    return response_handler(response)
+    return response.json()
+
 
 def handle_response_data():
-    number, messages = get_messages()
-    for i in range(len(messages)):
-        date = messages[i]['receivedAt']
-        pass
+    messages = get_messages()
+    today = date.today().isoformat()
+    for message in messages:
+        for number, data in message.items():
+            for item in data:
+                date_split = item['receivedAt'].split('T')
+                receive_date = date_split[0]
+                today = date.today().isoformat()
+                if receive_date == today:
+                    response_handler(send_message(number))
+                
 
 def send_message(user_number):
-    message = # call the method to get data from the json or NASA API
+    message = "Hello, here is the news for today + "# call the method to get data from the json of NASA API
     data = {
         "phoneNumber": user_number,
         "message": message,
-        "sender": "DONT_LOOK_UP"
+        "sender": APP_NAME
     }
     response = requests.post('http://hackathons.masterschool.com:3030/sms/send', json=data) 
-    return response_handler(response)
+    return response.status_code
 
-
-
-# response = requests.get("http://hackathons.masterschool.com:3030/team/getMessages/DONT_LOOK_UP")
-# print(response.text)
+handle_response_data()
