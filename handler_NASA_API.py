20dd21859e4a734bc14926ac2b4d1055202f9154
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-07-23 10:24:07 +0200

    02 Commit updates to handler_NASA_API.py

--- handler_NASA_API.py
+++ handler_NASA_API.py
@@ -5,35 +5,44 @@ API_KEY = 'yDf0pzhehgEcpalmhkRbaeNeo6CeWUeWSfB4QWRf'
 APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY
 
 
+def save_to_json(data, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+
 def fetch_data(url):
-    return requests.get(url)
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 def check_status(response):
-    return response.status_code == 200
+    if response.status_code == 200:
+        print("Message has been sent.")
+        return True
+    else:
+        print("Message hasn't been sent.")
+        return False
 
 
 def process_data(response_text):
     data_dict = json.loads(response_text)
+    processed_data = {}
     for key, value in data_dict.items():
         print(f"{key}:")
-        if isinstance(value, str) and '\n' in value: # we use this method to print multiple lines of text
-            for line in value.split('\n'):
-                print(f"    {line}")
-        else:
-            print(f"    {value}")
+        print(f"    {value}")
         print("-" * 50)
+        processed_data[key] = value
+    return processed_data
 
 
 if __name__ == '__main__':
-    date = input("Please enter a date in this format YYYY-MM-DD: ")
+    date_user_input = input("Please enter a date in YYYY-MM-DD format (or leave blank for today's picture): ")
     url = APOD_URL
-    if date:
-        url += f"&date={date}"
-    response = fetch_data(url)
-    if check_status(response):
-        process_data(response.text)
-    else:
-        print("Failed to fetch data:", response.status_code)
-
-
+    if date_user_input:
+        url += f"&date={date_user_input}"
+    
+    data = fetch_data(url)
+    filename = 'apod_data.json'
+    save_to_json(data, filename)
+    print(f"Data saved to {filename}")
