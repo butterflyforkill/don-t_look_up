22b57592093a501175917ba0f23a299efaed4be5
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-07-23 11:50:45 +0200

    03 Updatehandler_NASA_API.py to return data properly formated

--- handler_NASA_API.py
+++ handler_NASA_API.py
@@ -1,49 +1,47 @@
-# handle users answers
-# subscribe of the user
-# unsubscribe of the user
-# sending the SMS to the user
-
 import requests
-from datetime import date
-from dateutil import parser
+import json
 
-APP_NAME = "DONT_LOOK_UP"
-
-
-def response_handler(response):
-    if response == 200:
-        print("message has been sent")
-    else:
-        print("message hasn't been sent")
+API_KEY = 'yDf0pzhehgEcpalmhkRbaeNeo6CeWUeWSfB4QWRf'
+APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY
 
 
-def get_messages():
-    response = requests.get("http://hackathons.masterschool.com:3030/team/getMessages/DONT_LOOK_UP")
+def save_to_json(data, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def fetch_data(url):
+    response = requests.get(url)
+    response.raise_for_status()
     return response.json()
 
 
-def handle_response_data():
-    messages = get_messages()
-    today = date.today().isoformat()
-    for message in messages:
-        for number, data in message.items():
-            for item in data:
-                date_split = item['receivedAt'].split('T')
-                receive_date = date_split[0]
-                today = date.today().isoformat()
-                if receive_date == today:
-                    response_handler(send_message(number))
-
-
-def send_message(user_number):
-    message = "Hello, here is the news for today + "  # call the method to get data from the json of NASA API
-    data = {
-        "phoneNumber": user_number,
-        "message": message,
-        "sender": APP_NAME
+def fetch_data(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return {
+        'title': data.get('title', ''),
+        'description': data.get('explanation', ''),
+        'image': data.get('url', '')
     }
-    response = requests.post('http://hackathons.masterschool.com:3030/sms/send', json=data)
-    return response.status_code
 
 
-handle_response_data()
+def process_data(response_text):
+    data_dict = json.loads(response_text)
+    processed_data = {}
+    for key, value in data_dict.items():
+        print(f"{key}:")
+        print(f"    {value}")
+        print("-" * 50)
+        processed_data[key] = value
+    return processed_data
+
+
+date_user_input = input("Please enter a date in YYYY-MM-DD format (or leave blank for today's picture): ")
+url = APOD_URL
+if date_user_input:
+    url += f"&date={date_user_input}"
+
+data = fetch_data(url)
+print(data)
