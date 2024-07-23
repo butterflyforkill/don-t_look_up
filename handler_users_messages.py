9be61f5363b7768523e5e8966831856ebbe2dfd6
butterflyforkill <vasse.paul@gmail.com>
2024-07-23 15:33:45 +0200

    Add few changes for sending sms

--- handler_users_messages.py
+++ handler_users_messages.py
@@ -1,7 +1,7 @@
 import requests
 import json_parcer
 import handler_NASA_API
-from datetime import date
+from datetime import date, time
 
 
 APP_NAME = "DONT_LOOK_UP"
@@ -54,14 +54,14 @@ def handle_response_data():
     today = date.today().isoformat()
     for message in messages:
         if message['date'] == today and not check_user_receive_message(message['number'], today):
-            return response_handler(commands_handler(message['command'], message['number']))
+            return commands_handler(message['command'], message['number'], today)
         # else:
         #     return response_handler(commands_handler(message['command'], message['number']))
     return "Message has already been sent today"
 
 
-def send_message(user_number):
-    message_data = handler_NASA_API.get_nasa_data()
+def send_message(user_number, nasa_data_date):
+    message_data = handler_NASA_API.get_nasa_data(nasa_data_date)
     title = message_data['title']
     explanation = message_data['description'].split('.')
     image = message_data['image']
@@ -75,18 +75,32 @@ def send_message(user_number):
     return response.status_code
 
 
-def commands_handler(command, number):
-    """
-    Main function to run the program and interact with the user.
+def send_message_availble_commands(user_number):
+    message = ''
+    message += f"Hello dear user of the app Don't Look Up. " 
+    message += f"If you want to receive news from us in the different time of the day. Please send us a message with words:"
+    message += f" NASA POD and the time u want to receive news.\n"
+    message += f"For example: NASA POD 13"  
+    data = {
+        "phoneNumber": user_number,
+        "message": message,
+        "sender": APP_NAME
+    }
+    response = requests.post('http://hackathons.masterschool.com:3030/sms/send', json=data) 
+    return response.status_code
 
-    Returns:
-    None
-    """
+def commands_handler(command, number, today):
     menu_functionality = {
-            'SUBSCRIBE NASA': send_message
+            'SUBSCRIBE NASA': send_message,
+            'NASA POD': send_message
     }
     if command in menu_functionality:
-            return menu_functionality[command](number)
+        if command == 'SUBSCRIBE NASA':
+            availble_commands = response_handler(send_message_availble_commands(number))
+            send_msg = response_handler(menu_functionality[command](number, today))
+            return f"first_msg: {availble_commands}, second_msg: {send_msg}"
+        else:
+            return response_handler(menu_functionality[command](number, today))
     
 
 
